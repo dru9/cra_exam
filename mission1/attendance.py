dba8195d1c97f8c09e212cf5bff79ac073811bab
sjh <jh88.song@samsung.com>
2025-08-29 10:39:27 +0900

    3st

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,54 +1,45 @@
-id1 = {}
+id_by_names = {}
 id_cnt = 0
 
-# dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
 points = [0] * 100
 grade = [0] * 100
-names = [''] * 100
-wed = [0] * 100
-weeken = [0] * 100
+grade_names = ["NORMAL", "SILVER", "GOLD"]
+name_by_ids = [''] * 100
+wed_count = [0] * 100
+weekend_count = [0] * 100
 
-def input2(w, wk):
+def calculate_grade(person_id:int):
+    if wed_count[person_id] > 9:
+        points[person_id] += 10
+    if weekend_count[person_id] > 9:
+        points[person_id] += 10
+
+    if points[person_id] >= 50:
+        grade[person_id] = 2
+    elif points[person_id] >= 30:
+        grade[person_id] = 1
+    else:
+        grade[person_id] = 0
+
+def process_record(name:str, day:str):
     global id_cnt
 
-    if w not in id1:
+    if name not in id_by_names:
         id_cnt += 1
-        id1[w] = id_cnt
-        names[id_cnt] = w
+        id_by_names[name] = id_cnt
+        name_by_ids[id_cnt] = name
 
-    id2 = id1[w]
+    person_id = id_by_names[name]
 
-    add_point = 0
-    index = 0
+    if day in ["monday", "tuesday", "thursday", "friday"]:
+        points[person_id] += 1
+    elif day == "wednesday":
+        points[person_id] += 3
+        wed_count[person_id] += 1
+    elif day in ["saturday", "sunday"]:
+        points[person_id] += 2
+        weekend_count[person_id] += 1
 
-    if wk == "monday":
-        index = 0
-        add_point += 1
-    elif wk == "tuesday":
-        index = 1
-        add_point += 1
-    elif wk == "wednesday":
-        index = 2
-        add_point += 3
-        wed[id2] += 1
-    elif wk == "thursday":
-        index = 3
-        add_point += 1
-    elif wk == "friday":
-        index = 4
-        add_point += 1
-    elif wk == "saturday":
-        index = 5
-        add_point += 2
-        weeken[id2] += 1
-    elif wk == "sunday":
-        index = 6
-        add_point += 2
-        weeken[id2] += 1
-
-    dat[id2][index] += 1
-    points[id2] += add_point
 
 def input_file():
     try:
@@ -57,36 +48,20 @@ def input_file():
                 line = f.readline()
                 if not line:
                     break
-                parts = line.strip().split()
-                if len(parts) == 2:
-                    input2(parts[0], parts[1])
+                record = line.strip().split()
+                if len(record) == 2:
+                    process_record(name=record[0], day=record[1])
 
         for i in range(1, id_cnt + 1):
-            if dat[i][2] > 9:
-                points[i] += 10
-            if dat[i][5] + dat[i][6] > 9:
-                points[i] += 10
-
-            if points[i] >= 50:
-                grade[i] = 1
-            elif points[i] >= 30:
-                grade[i] = 2
-            else:
-                grade[i] = 0
-
-            print(f"NAME : {names[i]}, POINT : {points[i]}, GRADE : ", end="")
-            if grade[i] == 1:
-                print("GOLD")
-            elif grade[i] == 2:
-                print("SILVER")
-            else:
-                print("NORMAL")
+            calculate_grade(i)
+            print(f"NAME : {name_by_ids[i]}, POINT : {points[i]}, GRADE : ", end="")
+            print(grade_names[grade[i]])
 
         print("\nRemoved player")
         print("==============")
         for i in range(1, id_cnt + 1):
-            if grade[i] not in (1, 2) and wed[i] == 0 and weeken[i] == 0:
-                print(names[i])
+            if grade[i] not in (1, 2) and wed_count[i] == 0 and weekend_count[i] == 0:
+                print(name_by_ids[i])
 
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
